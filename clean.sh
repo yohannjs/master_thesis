1213b13497cf49dd0d26e8e6e30312848a15737d
Yohann Jacob Sandvik <yohannjsandvik@gmail.com>
2020-05-13 13:03:10 +0200

    Add data exp and case studies

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm master_thesis.aux
+rm master_thesis.bcf
+rm master_thesis.blg
+rm master_thesis.fdb_latexmk
+rm master_thesis.fls
+rm master_thesis.lof
+rm master_thesis.log
+rm master_thesis.lot
+rm master_thesis.out
+rm master_thesis.run.xml
+rm master_thesis.toc
