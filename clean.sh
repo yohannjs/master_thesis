6081a6cf5838a0b9067f4c4b46d967f5a84e0bf0
Yohann Jacob Sandvik <yohannjsandvik@gmail.com>
2020-05-25 15:00:56 +0200

    Add list of abbrev, and remove minted package.

--- clean.sh
+++ clean.sh
@@ -10,3 +10,7 @@ rm master_thesis.lot
 rm master_thesis.out
 rm master_thesis.run.xml
 rm master_thesis.toc
+rm master_thesis.acn*
+rm master_thesis.bbl*
+rm master_thesis.glo*
+rm master_thesis.ist*
--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-latexmk -pdf -shell-escape master_thesis.tex
+latexmk -pdf master_thesis.tex
+# latexmk -pdf -shell-escape master_thesis.tex
